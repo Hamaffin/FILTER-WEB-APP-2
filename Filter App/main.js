4d1a44d9cf58d9930aafb838810a1e9d146c3b71
Hamaffin <101374544+Hamaffin@users.noreply.github.com>
2022-09-29 20:40:06 -0400

    Add files via upload

---
+++ Filter App/main.js
@@ -0,0 +1,34 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide()
+  
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log("PoseNet is Initialized");
+}
+
+function gotPoses(results) {
+  console.log(results);
+  noseX = results[0].pose.nose.x-10;
+  noseY = results[0].pose.nose.y-10;
+  console.log("nose x = "+ results[0].pose.nose.x)
+  console.log("nose y = "+ results[0].pose.nose.y)
+}
+
+function draw() {
+    
+}
+
+function take_snapshot() {    
+  save('myFilterImage.png');
+}
